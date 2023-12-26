f84056e951f1c12e07e9da53b18abe5c14084a2a
GuginIlua <147326563+GuginIlua@users.noreply.github.com>
2023-12-26 17:49:15 +0300

    Add files via upload

---
+++ Doska.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int n=8;
+	for(int i=0;i<9*n;i++){
+		if(i%9==0){
+                        i++;
+                        printf("\n");
+               	};
+		if(i%2==0)
+                	printf("#");
+		if(i%2!=0)
+                	printf("_");
+	};
+	printf("\n");
+	return 0;
+}
+
